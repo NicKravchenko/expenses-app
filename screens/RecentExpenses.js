0fa39f32399365cc952dc9734f5cca583ec8b7a6
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-04-11 12:43:48 -0400

    Start styling expenses screen

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -2,11 +2,7 @@ import { View, Text } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return (
-    <View>
-      <ExpensesOutput expensesPeriod="Last 7 days" />
-    </View>
-  );
+  return <ExpensesOutput expensesPeriod="Last 7 days" />;
 }
 
 export default RecentExpenses;
